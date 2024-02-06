8a3bda3354f909c9b700274042b7a2e89f7787e7
asmoranomar <asmoranomar@gmail.com>
2024-02-06 03:07:21 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -64,6 +64,34 @@ function generateTasks() {
             '<i class="bi bi-gem"></i>'
           );
           listItemText = listItemText.replace(
+            /::item_ordinary::/g,
+            '<i class="bi bi-gem"></i>'
+          );
+                    listItemText = listItemText.replace(
+            /::item_common::/g,
+            '<i class="bi bi-gem"></i>'
+          );
+                    listItemText = listItemText.replace(
+            /::item_uncommon::/g,
+            '<i class="bi bi-gem text-success"></i>'
+          );
+                    listItemText = listItemText.replace(
+            /::item_rare::/g,
+            '<i class="bi bi-gem text-primary"></i>'
+          );
+                    listItemText = listItemText.replace(
+            /::item_veryrare::/g,
+            '<i class="bi bi-gem text-danger"></i>'
+          );
+                    listItemText = listItemText.replace(
+            /::item_legendary::/g,
+            '<i class="bi bi-gem text-warning"></i>'
+          );
+                    listItemText = listItemText.replace(
+            /::item_story::/g,
+            '<i class="bi bi-gem"></i>'
+          );
+          listItemText = listItemText.replace(
             /::ability::/g,
             '<i class="bi bi-mortarboard"></i>'
           );
